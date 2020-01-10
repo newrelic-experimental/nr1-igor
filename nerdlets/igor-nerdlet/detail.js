f6867d913f64bf7c5b3c8e973ec0fe2289c953bb
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2020-01-10 12:53:29 -0800

    checks for existence of host before fetching details on host

--- nerdlets/igor-nerdlet/detail.js
+++ nerdlets/igor-nerdlet/detail.js
@@ -101,7 +101,7 @@ export default class Detail extends React.Component {
   }
 
   pickedHost(host) {
-    this.fetchHost(host.id);
+    if (host && 'id' in host) this.fetchHost(host.id);
   }
 
   async fetchHost(guid) {
